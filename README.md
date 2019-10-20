7a3b13fd1d356148c76a801e91b485e0769777c1
caesarsalad <huseyin-cakir-2013@yandex.com>
2019-10-20 16:17:54 +0300

    update readme and upload code

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # eth-solo-pool
 Ethereum Solo Pool
+
+## Used in production for one year.
+### Only compatible with parity.
+### Nicehash supported with 64G diff (nicehash.json)
+
+To change block reward  payouts/unlocker.go#33 
+
+Use crontab syntax for time intervals of charts at api.json.
+
+You can use screen or tmux to open api, minerproxy,unlocker and payout modules separately. (at production I do not recommend to open all of them in one screen.)
+
+example:
+screen -mS api ./build/bin/open-ethereum-pool api.json
+
+# You can open issue on github for bugs. If you need help send e-mail to huseyin-cakir-2013@yandex.com.
+____________________
+Based on [sammy007/open-ethereum-pool](https://github.com/sammy007/open-ethereum-pool) 
---
+++ proxy/blocks.go
@@ -0,0 +1,99 @@
+package proxy
+
+import (
+	"log"
+	"math/big"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/common"
+
+	"github.com/feeleep75/open-ethereum-pool/rpc"
+	"github.com/feeleep75/open-ethereum-pool/util"
+)
+
+const maxBacklog = 3
+
+type heightDiffPair struct {
+	diff   *big.Int
+	height uint64
+}
+
+type BlockTemplate struct {
+	sync.RWMutex
+	Header               string
+	Seed                 string
+	Target               string
+	Difficulty           *big.Int
+	Height               uint64
+	GetPendingBlockCache *rpc.GetBlockReplyPart
+	nonces               map[string]bool
+	headers              map[string]heightDiffPair
+}
+
+type Block struct {
+	difficulty  *big.Int
+	hashNoNonce common.Hash
+	nonce       uint64
+	mixDigest   common.Hash
+	number      uint64
+}
+
+func (b Block) Difficulty() *big.Int     { return b.difficulty }
+func (b Block) HashNoNonce() common.Hash { return b.hashNoNonce }
+func (b Block) Nonce() uint64            { return b.nonce }
+func (b Block) MixDigest() common.Hash   { return b.mixDigest }
+func (b Block) NumberU64() uint64        { return b.number }
+
+func (s *ProxyServer) fetchBlockTemplate() {
+	r := s.rpc()
+	t := s.currentBlockTemplate()
+	reply, err := r.GetWork()
+	if err != nil {
+		log.Printf("Error while refreshing block template on %s: %s", r.Name, err)
+		return
+	}
+	// No need to update, we have fresh job
+	if t != nil && t.Header == reply[0] {
+		return
+	}
+
+	diff := util.TargetHexToDiff(reply[2])
+	height, err := strconv.ParseUint(strings.Replace(reply[3], "0x", "", -1), 16, 64)
+
+	pendingReply := &rpc.GetBlockReplyPart{
+		Difficulty: util.ToHex(s.config.Proxy.Difficulty),
+		Number:     reply[3],
+	}
+
+	newTemplate := BlockTemplate{
+		Header:               reply[0],
+		Seed:                 reply[1],
+		Target:               reply[2],
+		Height:               height,
+		Difficulty:           diff,
+		GetPendingBlockCache: pendingReply,
+		headers:              make(map[string]heightDiffPair),
+	}
+	// Copy job backlog and add current one
+	newTemplate.headers[reply[0]] = heightDiffPair{
+		diff:   diff,
+		height: height,
+	}
+	if t != nil {
+		for k, v := range t.headers {
+			if v.height > height-maxBacklog {
+				newTemplate.headers[k] = v
+			}
+		}
+	}
+	s.blockTemplate.Store(&newTemplate)
+	log.Printf("New block to mine on %s at height %d / %s / %d", r.Name, height, reply[0][0:10], diff)
+
+	// Stratum
+	if s.config.Proxy.Stratum.Enabled {
+		go s.broadcastNewJobs()
+	}
+}
+
